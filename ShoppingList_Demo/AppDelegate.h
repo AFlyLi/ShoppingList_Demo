60df443c4f6d63a585c61a745c7ef5c19093ed30
李金华 <lijinhua@lijinhuadeMacBook-Pro.local>
2015-04-20 15:23:27 +0800

    Initial Commit

---
+++ ShoppingList_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingList_Demo
+//
+//  Created by 李金华 on 15/4/20.
+//  Copyright (c) 2015年 LJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
